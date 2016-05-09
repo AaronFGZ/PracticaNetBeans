a476026ccb9c558df32ea4ba6f0eaa7a55606563
A15AaronFG <A15AaronFG@a21eq29.sanclemente.local>
2016-05-09 19:12:24 +0200

    "Creacion Proyecto

---
+++ PracticaNBGit/src/practicanbgit/PracticaNBGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicanbgit;
+
+/**
+ *
+ * @author A15AaronFG
+ */
+public class PracticaNBGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
